799accf713f36465cccc506e5ac3891993712bd6
PranavSikarwal <sikarwalpranav123@gmail.com>
2023-12-05 12:59:44 +0530

    try package.json fix

--- Client/src/shared/components/UIElements/map.jsx
+++ Client/src/shared/components/UIElements/map.jsx
@@ -12,7 +12,7 @@ const Map = (props) => {
 
       map.current = new maplibregl.Map({
         container: mapElement.current,
-        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.REACT_APP_MAPLIBREGL_KEY}`,
+        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=t3MEXMGv64kn2RuQv7FJ`,
         center: [lng, lat],
         zoom: 15,
       });
